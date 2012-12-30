e58787f75a850943f3fde322754930b16acf148d
Daniel Werdermann <dwerdermann@web.de>
2012-12-30 14:30:55 +0100

    just update license information

--- check_user_implementation.sh
+++ check_user_implementation.sh
@@ -1,25 +1,16 @@
+
 #!/bin/sh
 
 # --------------------------------------------------------------------
 # **** BEGIN LICENSE BLOCK *****
 #
-# Version: MPL 1.1
+# Version: MPL 2.0
 #
-# The contents of this file are subject to the Mozilla Public License Version
-# 1.1 (the "License"); you may not use this file except in compliance with
-# the License. You may obtain a copy of the License at
-# http://www.mozilla.org/MPL/
+# echocat check_user_implementation.sh, Copyright (c) 2011-2012 echocat
 #
-# Software distributed under the License is distributed on an "AS IS" basis,
-# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
-# for the specific language governing rights and limitations under the
-# License.
-#
-# The Original Code is echocat management.
-#
-# The Initial Developer of the Original Code is Daniel Werdermann.
-# Portions created by the Initial Developer are Copyright (C) 2011
-# the Initial Developer. All Rights Reserved.
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 # **** END LICENSE BLOCK *****
 # --------------------------------------------------------------------
@@ -28,9 +19,12 @@
 # Check if a specified user exists and that he has the correct userId and groupId.
 #
 # @author: Daniel Werdermann / dwerdermann@web.de
-# @veriosn: 1.2
-# @date: 2011-11-04 17:50:23Z
+# @projectsite: https://github.com/echocat/nagios-plugin-check_userimplementation
+# @veriosn: 1.3
+# @date: 2012-12-30 14:21:44Z
 #
+# changes 1.3
+#  - update license information
 # changes 1.2
 #  - add license information
 # changes 1.1
